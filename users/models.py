9b1abc75a7ebd6c770c7473f16f37a64f407df45
51193 <2878740067@qq.com>
2023-05-16 18:54:48 +0800

    test

--- users/models.py
+++ users/models.py
@@ -1,4 +1,53 @@
 from django.db import models
 
+
 # Create your models here.
 
+class User(models.Model):
+    gender = [
+        ("m", "男"),
+        ("f", "女")
+    ]
+
+    nick_name = models.CharField(max_length=50, verbose_name="昵称")
+    gender = models.CharField(max_length=10, choices=gender, default='m', verbose_name="性别")
+    email = models.EmailField(verbose_name="邮箱")
+
+    client_number = models.CharField(max_length=20, verbose_name="账号")
+    client_password = models.CharField(max_length=20, verbose_name="密码")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['client_number'], name='client_number'),
+        ]
+
+    def get_id(self):
+        return self.client_number
+
+    def __str__(self):
+        return "%s (%s)" % (self.client_number, self.nick_name)
+
+
+class Admin(models.Model):
+    gender = [
+        ("m", "男"),
+        ("f", "女")
+    ]
+
+    nick_name = models.CharField(max_length=50, verbose_name="昵称")
+    gender = models.CharField(max_length=10, choices=gender, default='m', verbose_name="性别")
+    email = models.EmailField(verbose_name="邮箱")
+
+    admin_number = models.CharField(max_length=20, verbose_name="账号")
+    admin_password = models.CharField(max_length=20, verbose_name="密码")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['client_number'], name='client_number'),
+        ]
+
+    def get_id(self):
+        return self.client_number
+
+    def __str__(self):
+        return "%s (%s)" % (self.client_number, self.nick_name)
